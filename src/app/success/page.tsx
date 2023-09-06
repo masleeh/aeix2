8abac906200fcdb5b010e92d18b8d9912212d886
masleeh <102211370+masleeh@users.noreply.github.com>
2023-09-07 01:32:48 +0300

    test commit

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Common/Navbar/Navbar"
 import SuccessModal from "@/components/SuccessPage/SuccessModal/SuccessModal"
+import React from "react"
 
 const page = () => {
     return (
-        <>
+        <React.Fragment>
             <Navbar />
             <SuccessModal />
-        </>
+        </React.Fragment>
     )
 }
 
